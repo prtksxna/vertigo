a4496bf4eca4ebe632e52f4a4b501a5d870bfee9
Prateek Saxena <prtksxna@gmail.com>
2013-03-24 15:03:38 +0530

    Controls for touch devices

--- control.js
+++ control.js
@@ -1,19 +1,43 @@
 var setupControls = function(game){
-    $(document).keydown(function(e){
+    var lr = $(window).width() / 2;
+
+    $(document).bind("keydown",function(e){
         switch(e.keyCode){
         case 37:
-            e.preventDefault;
+            e.preventDefault();
             game.event = "left";
             break;
         case 39:
-            e.preventDefault;
+            e.preventDefault();
             game.event = "right";
             break;
         default:
             // Do nothing
         };
     });
-    $(document).keyup(function(e){
+
+    $(document).bind("keyup",function(e){
+        game.event = "";
+    });
+
+    $(document).bind("mousedown touchstart",function(e){
+        e.preventDefault();
+        var x = 0;
+
+        if (e.originalEvent.touches){
+            x = e.originalEvent.touches[0].pageX;
+        }else{
+            x = e.pageX;
+        }
+
+        if (x > lr){
+            game.event = "right";
+        }else{
+            game.event = "left";
+        }
+    });
+
+    $(document).bind("mouseup touchend",function(e){
         game.event = "";
     });
 };
